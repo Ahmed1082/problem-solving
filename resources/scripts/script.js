c0966a4613e122ca62aa292048a509112d6099eb
Ahmed1082 <a7medsalama12345@gmail.com>
2024-08-11 22:04:32 +0300

    Task Eng/Ahmed

---
+++ resources/scripts/script.js
@@ -0,0 +1,89 @@
+// question 1
+
+// let number = prompt("Enter a number:");
+// if (number <= 100) {
+//     console.log("Number is less than or equal to 100");
+// } else {
+//     console.log("Number is greater than 100");
+// }
+
+// question 2
+
+// let text = prompt("Enter text:");
+// if (text.length > 100) {
+//     console.log("The string is long");
+// } else {
+//     console.log("The string is short");
+// }
+
+// question 3
+
+// let password = prompt("Enter your password:");
+// let confirm = prompt("Confirm your password:");
+// if (password === confirm) {
+//     console.log("Password is valid");
+// } else {
+//     console.log("Password is not valid");
+// }
+
+// question 4
+
+// let day = prompt("Enter the day:");
+// if (day === "Saturday") {
+//     console.log("It's the weekend");
+// } else {
+//     console.log("It's a workday");
+// }
+
+// question 6
+
+// let grade = prompt("Enter your grade:");
+// if (grade >= 90 ){
+//     console.log("grade is A");
+// }
+// else if (grade >= 80 ){
+//     console.log("grade is B");
+// }
+// else if (grade >= 70 ){
+//     console.log("grade is C");
+// }
+// else if (grade >= 60 ){
+//     console.log("grade is D");
+// }
+// else {
+//     console.log("grade is F");
+// }
+
+// question 7
+
+// let temp = prompt("Enter the temperature:");
+// if (temp > 30) {
+//     console.log("It's hot");
+// } else {
+//     console.log("It's cold");
+// }
+
+// question 8
+
+// let number = prompt("Enter a number:");
+// if (number % 2 === 0) {
+//     console.log("Number is even");
+// } else {
+//     console.log("Number is odd");
+// }
+
+// question 9
+
+// let number = prompt("Enter a number:");
+// if (number > 20) {
+//     if (number % 2 === 0) {
+//         console.log("Even and greater than 20, you can enter.");
+//     } else {
+//         console.log("Odd and greater than 20, you can enter.");
+//     }
+// } else {
+//     console.log("you can't enter.");
+// }
+
+
+
